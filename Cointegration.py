6073adf0e48e1ba5bab12b1384da2af721f0eb5f
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 20:21:42 +0800

    feat: add func cointegration_test()

--- Cointegration.py
+++ Cointegration.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from itertools import combinations
+from statsmodels.tsa.stattools import coint
 
 def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
     normalized_prices_df:pd.DataFrame = (prices_df-prices_df.mean())/prices_df.std()
@@ -14,6 +15,25 @@ def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
     selection_ranked = selection.set_index('pair').sort_values('MSD')
     
     return selection_ranked
+
+def cointegration_test(prices_df: pd.DataFrame, alpha: float=0.05) -> pd.DataFrame:
+    pair_list = []
+    score_list = []
+    pvalue_list = []
+    cointegration_list = []
+    for pair in combinations(prices_df.columns, 2):
+        pair_list.append(str(pair))
+        score, pvalue, _ = coint(
+            prices_df.loc[:, pair[0]], prices_df.loc[:, pair[1]])
+        cointegration = True if (pvalue <= alpha) else False
+        score_list.append(score)
+        pvalue_list.append(pvalue)
+        cointegration_list.append(cointegration)
+    result_df = pd.DataFrame({'pair': pair_list, 'score': score_list,
+                             'p-value': pvalue_list, 'cointegration': cointegration_list})
+    result_df_ranked = result_df.set_index('pair').sort_values('p-value')
+
+    return result_df_ranked
 
 def log_return(prices_df:pd.DataFrame) -> pd.DataFrame:
     rtn_df = pd.DataFrame()
