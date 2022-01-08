66b918c754066a3a372a06b092e7d89eb3444266
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 19:38:03 +0800

    refactor: define normalized_prices_df as pd.DataFrame

--- Cointegration.py
+++ Cointegration.py
@@ -2,9 +2,8 @@ import pandas as pd
 import numpy as np
 from itertools import combinations
 
-
 def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
-    normalized_prices_df = (prices_df-prices_df.mean())/prices_df.std()
+    normalized_prices_df:pd.DataFrame = (prices_df-prices_df.mean())/prices_df.std()
     pair_list = []
     MSD_list  = []
     for pair in combinations(normalized_prices_df.columns,2):
@@ -18,7 +17,7 @@ def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
 
 def log_return(prices_df:pd.DataFrame) -> pd.DataFrame:
     rtn_df = pd.DataFrame()
-    for crypto in list(prices_df.columns):
+    for crypto in prices_df.columns:
         rtn_df['crypto'] = np.log(prices_df.loc[:,crypto] / prices_df.loc[:,crypto].shift(1))
 
     return rtn_df
