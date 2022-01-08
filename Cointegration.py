0649c32ad8c5b4b3d888aa08219359e362ba70cd
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 19:25:01 +0800

    feat: add func pair_selection_MSD()

--- Cointegration.py
+++ Cointegration.py
@@ -1,5 +1,20 @@
 import pandas as pd
 import numpy as np
+from itertools import combinations
+
+
+def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
+    normalized_prices_df = (prices_df-prices_df.mean())/prices_df.std()
+    pair_list = []
+    MSD_list  = []
+    for pair in combinations(normalized_prices_df.columns,2):
+        pair_list.append(str(pair))
+        MSD_list.append(sum((normalized_prices_df.loc[:,pair[0]] - normalized_prices_df.loc[:,pair[1]]) ** 2))
+
+    selection = pd.DataFrame({'pair':pair_list, 'MSD':MSD_list})
+    selection_ranked = selection.set_index('pair').sort_values('MSD')
+    
+    return selection_ranked
 
 def log_return(df:pd.DataFrame) -> pd.DataFrame:
     rtn_df = pd.DataFrame()
