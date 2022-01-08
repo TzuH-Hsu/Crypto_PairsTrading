765757df664e30d857a2416814a3c0714da96b5f
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 19:39:18 +0800

    fix: log_return return dataframe column

--- Cointegration.py
+++ Cointegration.py
@@ -18,6 +18,6 @@ def pair_selection_MSD(prices_df:pd.DataFrame) -> pd.DataFrame:
 def log_return(prices_df:pd.DataFrame) -> pd.DataFrame:
     rtn_df = pd.DataFrame()
     for crypto in prices_df.columns:
-        rtn_df['crypto'] = np.log(prices_df.loc[:,crypto] / prices_df.loc[:,crypto].shift(1))
+        rtn_df[f'{crypto}'] = np.log(prices_df.loc[:,crypto] / prices_df.loc[:,crypto].shift(1))
 
     return rtn_df
