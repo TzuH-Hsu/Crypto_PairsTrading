69a89dad665ede81add19501bc6ae2ce5d0f3a91
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 17:30:21 +0800

    feat: log_return() input as dataframe

--- Cointegration.py
+++ Cointegration.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
 
-def log_return(series:pd.Series) -> pd.Series:
-    rtn = np.log(series / series.shift(1))
+def log_return(df:pd.DataFrame) -> pd.DataFrame:
+    rtn_df = pd.DataFrame()
+    for crypto in list(df.columns):
+        rtn_df['crypto'] = np.log(df.loc[:,crypto] / df.loc[:,crypto].shift(1))
 
-    return rtn
+    return rtn_df
