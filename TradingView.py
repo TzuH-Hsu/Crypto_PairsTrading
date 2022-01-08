154d98fc4f650d8c7f131d0f49380e5420ffa5a7
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 14:10:11 +0800

    fix: total days count from date

--- TradingView.py
+++ TradingView.py
@@ -9,8 +9,8 @@ class Datafeed:
         
         date_start = datetime.strptime(date_start_str, '%Y/%m/%d')
         date_end   = datetime.strptime(date_end_str  , '%Y/%m/%d')
-        delta = date_end - datetime.now()
-        data_extended = self.tv.get_hist(symbol=symbol, exchange='BINANCE', interval=Interval.in_daily, n_bars=(delta.days + 100))
+        delta = date_start - datetime.now()
+        data_extended = self.tv.get_hist(symbol=symbol, exchange='BINANCE', interval=Interval.in_daily, n_bars=(delta.days + 90))
         data = data_extended[date_start:date_end]
 
         return data.close
