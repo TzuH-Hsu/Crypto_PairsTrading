fea5d43af5db6dbc463b1ddfeba7f06bc80f0d4a
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-08 14:42:05 +0800

    feat: let 'exchange' as argument

--- TradingView.py
+++ TradingView.py
@@ -1,16 +1,17 @@
 from datetime import datetime
+from pandas import Series
 from tvDatafeed import TvDatafeed, Interval
 
 class Datafeed:
     def __init__(self) -> None:
         self.tv = TvDatafeed()
         
-    def Binance_daily_close(self, symbol:str, date_start_str:str, date_end_str:str):
+    def daily_close(self, symbol:str, exchange:str, date_start_str:str, date_end_str:str) -> Series:
         
         date_start = datetime.strptime(date_start_str, '%Y/%m/%d')
         date_end   = datetime.strptime(date_end_str  , '%Y/%m/%d')
-        delta = date_start - datetime.now()
-        data_extended = self.tv.get_hist(symbol=symbol, exchange='BINANCE', interval=Interval.in_daily, n_bars=(delta.days + 90))
+        delta = datetime.now() - date_start
+        data_extended = self.tv.get_hist(symbol=symbol, exchange=exchange, interval=Interval.in_daily, n_bars=(delta.days + 90))
         data = data_extended[date_start:date_end]
 
         return data.close
