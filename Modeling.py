46424a5935164e31be524700c7ecfcdd593483e8
Tzuhsuan Hsu <arthur@cpnet.tw>
2022-01-09 14:34:30 +0800

    feat: calulate pairs return spreads

--- Modeling.py
+++ Modeling.py
@@ -1,5 +1,7 @@
+from numpy.lib.function_base import append
 import pandas as pd
 import numpy as np
+from itertools import combinations
 
 def log_return(prices_df:pd.DataFrame) -> pd.DataFrame:
     rtn_df = pd.DataFrame()
@@ -7,3 +9,14 @@ def log_return(prices_df:pd.DataFrame) -> pd.DataFrame:
         rtn_df[f'{crypto}'] = np.log(prices_df.loc[:,crypto] / prices_df.loc[:,crypto].shift(1))
 
     return rtn_df
+
+def return_spreads(return_df:pd.DataFrame) -> pd.DataFrame:
+    dfs = []
+    pair_list=[]
+    for pair in combinations(return_df.columns, 2):
+        spread_tmp = return_df.loc[:, pair[0]] - return_df.loc[:, pair[1]]
+        dfs.append(spread_tmp)
+        pair_list.append(str(pair))
+    return_spreads_df = pd.concat(dfs, axis='columns', keys=pair_list)
+
+    return return_spreads_df
